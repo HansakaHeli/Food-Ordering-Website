b8dd698a472fcc33f6c657e4dfe2378b93100492
HansakaHeli <programminghhr66@gmail.com>
2024-05-21 15:54:49 +0530

    Create RestaurantRepository.java

---
+++ backend-spring-boot/Online Food Ordering/Online-Food-Ordering/src/main/java/com/heli/repository/RestaurantRepository.java
@@ -0,0 +1,15 @@
+package com.heli.repository;
+
+import com.heli.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
+
+    @Query("SELECT r FROM Restaurant r WHERE lower(r.name) LIKE lower(concat('%', :query, '%')) " + "OR lower(r.cuisineType) LIKE (concat('%', :query, '%') ) ")
+    List<Restaurant> findBySearchQuery(String query);
+    Restaurant findByOwnerId(int userId);
+
+}
