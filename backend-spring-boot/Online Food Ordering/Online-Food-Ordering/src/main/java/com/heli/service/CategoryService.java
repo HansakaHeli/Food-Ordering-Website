302a3a9ec69e431338454c34f65313847d44205b
HansakaHeli <programminghhr66@gmail.com>
2024-07-03 18:21:29 +0530

    Create CategoryService

---
+++ backend-spring-boot/Online Food Ordering/Online-Food-Ordering/src/main/java/com/heli/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.heli.service;
+
+import com.heli.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    public Category createCategory(String name, int userId);
+
+    public List<Category> findCategoryByRestaurantId(int id)throws Exception;
+
+    public Category findCategoryById(int id)throws Exception;
+}
---
+++ backend-spring-boot/Online Food Ordering/Online-Food-Ordering/src/main/java/com/heli/service/CategoryServiceImpl.java
@@ -0,0 +1,22 @@
+package com.heli.service;
+
+import com.heli.model.Category;
+
+import java.util.List;
+
+public class CategoryServiceImpl implements CategoryService{
+    @Override
+    public Category createCategory(String name, int userId) {
+        return null;
+    }
+
+    @Override
+    public List<Category> findCategoryByRestaurantId(int id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Category findCategoryById(int id) throws Exception {
+        return null;
+    }
+}
