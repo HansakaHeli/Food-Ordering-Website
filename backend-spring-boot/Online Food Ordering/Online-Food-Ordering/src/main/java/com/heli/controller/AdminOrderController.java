8f717c90b4a7123a8048b5cc7d6481fbc4b0c3f3
HansakaHeli <programminghhr66@gmail.com>
2024-07-08 11:54:38 +0530

    Create AdminOrderController

---
+++ backend-spring-boot/Online Food Ordering/Online-Food-Ordering/src/main/java/com/heli/controller/AdminOrderController.java
@@ -0,0 +1,46 @@
+package com.heli.controller;
+
+import com.heli.model.Order;
+import com.heli.model.User;
+import com.heli.request.OrderRequest;
+import com.heli.service.OrderService;
+import com.heli.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminOrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/order/restaurant/{id}")
+    public ResponseEntity<List<Order>> getOrderHistory(@PathVariable int id,
+                                                       @RequestParam(required = false) String order_status,
+                                                       @RequestHeader("Authorization") String jwt)throws Exception{
+
+        User user = userService.findUserByJwtToken(jwt);
+        List<Order> orders = orderService.getRestaurantOrder(id,order_status);
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+
+    }
+
+    @PutMapping("/order/{id}/{orderStatus}")
+    public ResponseEntity<Order> updateOrderStatus(@PathVariable int id,
+                                                       @PathVariable String orderStatus,
+                                                       @RequestHeader("Authorization") String jwt)throws Exception{
+
+        User user = userService.findUserByJwtToken(jwt);
+        Order orders = orderService.updateOrder(id,orderStatus);
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+
+    }
+}
